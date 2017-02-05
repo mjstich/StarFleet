b3acd7621d6577b9b660a7cecb313ad9fff8afb7
Marty Stich <mjstich@gmail.com>
2017-02-04 23:47:22 -0600

    convert tabs to spaces.

--- Script.java
+++ Script.java
@@ -7,96 +7,96 @@ import java.util.stream.Stream;
 
 public class Script {
 
-	private List<String> commands;
+    private List<String> commands;
 
-	private Script(List<String> commands) {
-		this.commands = commands;
-	}
+    private Script(List<String> commands) {
+        this.commands = commands;
+    }
 
-	public static Script fromFile(String filename) throws IOException {
-		final List<String> commands = new ArrayList<String>();
-		try (Stream<String> stream = Files.lines(Paths.get(filename))) {
+    public static Script fromFile(String filename) throws IOException {
+        final List<String> commands = new ArrayList<String>();
+        try (Stream<String> stream = Files.lines(Paths.get(filename))) {
             stream.forEach(line -> {
-            	commands.add(line);
+                commands.add(line);
             });
             return new Script(commands);
         }
-	}
+    }
 
-	public void run(Field field) {
-		System.out.println("");
-		int mineCount = field.getMineCount();
-		int shotsFired = 0;
-		int moves = 0;
-		boolean movesRemaining = false;
+    public void run(Field field) {
+        System.out.println("");
+        int mineCount = field.getMineCount();
+        int shotsFired = 0;
+        int moves = 0;
+        boolean movesRemaining = false;
 
-		for (int i = 0; i < commands.size(); i++) {
-			String line = commands.get(i);
-			System.out.println("Step " + (i+1) + "\n");		
-				
-			if (line.trim().length() == 0) {
-				field.drop();
-			}
-			else {
-				System.out.println(field.toString());
-				String[] tokens = line.split(" ");
-				Direction direction = null;
-				FiringPattern firingPattern = null;	
+        for (int i = 0; i < commands.size(); i++) {
+            String line = commands.get(i);
+            System.out.println("Step " + (i+1) + "\n");     
+                
+            if (line.trim().length() == 0) {
+                field.drop();
+            }
+            else {
+                System.out.println(field.toString());
+                String[] tokens = line.split(" ");
+                Direction direction = null;
+                FiringPattern firingPattern = null; 
 
-				for (int j = 0; j < tokens.length; j++) {
-					try {
-						firingPattern = FiringPattern.valueOf(tokens[j]);
-					} 
-					catch (IllegalArgumentException e) {
-					}
+                for (int j = 0; j < tokens.length; j++) {
+                    try {
+                        firingPattern = FiringPattern.valueOf(tokens[j]);
+                    } 
+                    catch (IllegalArgumentException e) {
+                    }
 
-					if (firingPattern != null) {
-						field.fire(firingPattern);	
-						shotsFired++;	
-					}					
+                    if (firingPattern != null) {
+                        field.fire(firingPattern);  
+                        shotsFired++;   
+                    }                   
 
-					try {
-						direction = Direction.valueOf(tokens[j]);
-					}
-					catch (IllegalArgumentException e) {
-					}
+                    try {
+                        direction = Direction.valueOf(tokens[j]);
+                    }
+                    catch (IllegalArgumentException e) {
+                    }
 
-					if (direction != null) {
-						field.move(direction);
-						moves++;
-					}					
-				}
-				if (firingPattern != null) {
-					System.out.print(firingPattern + " ");
-				}
-				if (direction != null) {
-					System.out.println(direction);
-				}
-				System.out.println("");
-				field.drop();
-				System.out.println(field.toString());
-			}
+                    if (direction != null) {
+                        field.move(direction);
+                        moves++;
+                    }                   
+                }
+                if (firingPattern != null) {
+                    System.out.print(firingPattern + " ");
+                }
+                if (direction != null) {
+                    System.out.println(direction);
+                }
+                System.out.println("");
+                field.drop();
+                System.out.println(field.toString());
+            }
 
-			if (!field.anyActiveMines() && (i+1) < commands.size()) {
-				movesRemaining = true;
-				break;
-			}
-		}
+            if (!field.anyActiveMines() && (i+1) < commands.size()) {
+                movesRemaining = true;
+                break;
+            }
+        }
 
-		if (field.anyRemainingMines()) {
-			System.out.println("fail (0)");
-		}
-		else {
-			int score;
-			if (movesRemaining) {
-				score = 1;
-			}
-			else {
-				shotsFired = Math.min((shotsFired * 5), (mineCount * 5));
-			    moves = Math.min((moves * 2), (mineCount * 3));
-			    score = (mineCount * 10) - shotsFired - moves;
-			}
-			System.out.printf("pass (%d)\n", score);
-		}
-	}
+        if (field.anyRemainingMines()) {
+            System.out.println("fail (0)");
+        }
+        else {
+            int score;
+            if (movesRemaining) {
+                score = 1;
+            }
+            else {
+                shotsFired = Math.min((shotsFired * 5), (mineCount * 5));
+                moves = Math.min((moves * 2), (mineCount * 3));
+                score = (mineCount * 10) - shotsFired - moves;
+            }
+            System.out.printf("pass (%d)\n", score);
+        }
+    }
 }
